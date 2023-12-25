a8df8f17dab2e2ffba7d65d95a56b24f743297a5
Nitin Sapakal <Sapkalnitin500@gmail.com>
2023-12-25 14:09:04 +0530

    added

---
+++ Frontend_Autopartshub/src/pages/AboutUs.js
@@ -0,0 +1,41 @@
+
+
+const AboutUs = () => {
+  return (
+<div className="container mt-5">
+<div className="row">
+  <div className="col-md-6 offset-md-3">
+    <div className="bg-light p-4">
+     
+      <p>
+      Online shopping is a process whereby consumers directly buy goods, services etc. from a seller without an intermediary service over the Internet. Shoppers can visit web stores from the comfort of their house and shop as by sitting in front of the computer.Ecommerce, also known as AutoPartHUB commerce or internet commerce, refers to the buying and selling of goods or services using the internet, and the transfer of money and data to execute these transactions.
+
+      </p>
+      <ul className="list-unstyled">
+        <li>
+          <strong></strong><br />
+          In existing system shopping can done in a manual way, the customer has to go for shopping, and then he is having the possibility to choose the products what ever he wants. Selling online also has its advantages when it comes to convincing customers you're the best in the industry. Your website can inform customers about your sales, the quality of your products, and why they should buy from you. You can also show customer reviews, so people know they're buying from a reputable brand. E-commerce (EC), an abbreviation for AutoPartsHUB commerce, is the buying and selling of goods and services, or the transmitting of funds or data, over an SpareParts network, primarily the internet.
+
+        </li>
+        <li className="mt-3">
+          <strong></strong><br />
+          The online shopping system is fast gaining media for to sale or purchase items from anywhere and anytime. It is basically based on Internet, It is related with B2C (Business to Customer) model and status of the design and development of e-commerce platform.E-business or Online business means business transactions that take place online with the help of the internet. The term e-business came into existence in the year 1996.  E-business is an abbreviation for SpareParts business. Therefore, the buyer and the seller do not meet personally. E-commerce is directly link to your business promotions, as it is the age of digital media. Making your business available online is crucial to your business development such as, highly convenience, wide exposure, global customer, easy to run, etc. 
+
+        </li>
+      </ul>
+    </div>
+  </div>
+</div>
+</div>
+ 
+   
+
+
+
+
+
+
+  );
+};
+
+export default AboutUs;
---
+++ Frontend_Autopartshub/src/pages/ContactUs.js
@@ -0,0 +1,31 @@
+const ContactUs = () => {
+  return (
+      <div className="container mt-5">
+        <div className="row">
+          <div className="col-md-6 offset-md-3">
+            <div className="bg-light p-4">
+              <h2>Contact Us</h2>
+              <p>
+                For any inquiries, please feel free reach out to us:
+              </p>
+              <ul className="list-unstyled">
+                <li>
+                  <strong>Nitin Sapakal</strong><br />
+                  Email:   nitin2000@gmail.com
+                </li>
+                <li className="mt-3">
+                  <strong>Bhagyesh Lunawat</strong><br />
+                  Email:   bhagyesh2000@gmail.com
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+  
+
+
+  );
+};
+
+export default ContactUs;
